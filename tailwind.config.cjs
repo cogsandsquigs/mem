6fa67d71c00238127e2ab1f4d5bfee9a7364d924
Ian Pratt <thingcoder1@gmail.com>
2022-06-02 14:17:51 -0500

    yay added things

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,25 @@
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  
+  // REMINDER: theme can be generated from daisyui.
+  // Copy-paste theme colors into tailwind colors
+  // section
   theme: {
-    extend: {},
+    colors: {
+      primary: "#111827",
+      secondary: "#3ABFF8",
+      accent: "#2563EB",
+      neutral: "#111827",
+      "base-100": "#111827",
+      info: "#3ABFF8",
+      success: "#36D399",
+      warning: "#FBBD23",
+      error: "#F87272",
+    },
   },
-  plugins: [],
-}
+  
+  plugins: [
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms'),
+  ],
+};
