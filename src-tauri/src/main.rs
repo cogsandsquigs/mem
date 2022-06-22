4385bc18f6bd04e7b0060f49d79684e649233b89
Ian Pratt <ianjdpratt@gmail.com>
2022-06-22 14:46:52 -0500

    asdfasdfasdf

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,40 +3,12 @@
     windows_subsystem = "windows"
 )]
 
-use db::Database;
-use tauri::Manager;
-
-pub mod card;
 pub mod db;
-pub mod user;
 
 fn main() {
     tauri::Builder::default()
-        .setup(|app| {
-            // listen to the `event-name` (emitted on any window)
-            let id = app.listen_global("add-card", |event| {
-                let p = event.payload().unwrap();
-                println!("got event-name with payload {:?}", p);
-                let mut db = Database::new("./data/db.json");
-                db.add_card(serde_json::from_slice(p.as_bytes()).unwrap())
-            });
-            // unlisten to the event using the `id` returned on the `listen_global` function
-            // an `once_global` API is also exposed on the `App` struct
-            // app.unlisten(id);
-            // emit the `event-name` event to all webview windows on the frontend
-            /*
-            app.emit_all(
-                "event-name",
-                Card {
-                    message: "Tauri is awesome!".into(),
-                },
-            )
-            .unwrap();
-            */
-            Ok(())
-        })
         // This is where you pass in your commands
-        .invoke_handler(tauri::generate_handler![db::get_new_db])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import { invoke } from "@tauri-apps/api/tauri";
-  import { emit, listen } from "@tauri-apps/api/event";
-
-  //import dayjs from "dayjs";
-
-  // where all the logic goes
-  onMount(async () => {
-    // listen to the `click` event and get a function to remove the event listener
-    // there's also a `once` function that subscribes to an event and automatically unsubscribes the listener on the first event
-    const unlisten = await listen("add-card", (event) => {
-      // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
-      // event.payload is the payload object
-    });
-
-    // emits the `click` event with the object payload
-    emit("add-card", {
-      id: 0,
-      topic: "", // the cards' topic
-      front: "", // the cards' front face, as a markdown string
-      back: "", // the cards' back face, as a markdown string
-      bucket: 0, // the bucket the card is in - used for the leitner system
-      date_created: "",
-    });
-  });
 </script>
 
 <h1>mem</h1>
